33eb51b0b9830380acfff7e7cd0d495be943e261
Erland Lie Amundgård <erlandla@stud.ntnu.no>
2023-10-11 15:44:25 +0200

    test out time series decomposition

---
+++ TimeSeriesDecompTest.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
+from matplotlib import pyplot as plt
+
+data = pd.read_csv("Tetuan City power consumption.csv")
+
+data.set_index("DateTime", inplace=True)
+data.index = pd.to_datetime(data.index)
+
+# Drop null values
+data.dropna(inplace=True)
+
+result = seasonal_decompose(data["Zone 1 Power Consumption"], model="multiplicative", period=1440)
+result.seasonal.plot()
+result.trend.plot()
+fig = result.plot()
+plt.show()

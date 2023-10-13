7f0ad430d841a06cc89f111a2e19c08b219dee0e
Tommy Chan <tommychan661995@gmail.com>
2023-10-13 11:28:45 +0200

    add mockdata generation and function for normalization

---
+++ preprocessing/mockData/generateMockData.py
@@ -0,0 +1,18 @@
+import random
+import pandas as pd
+
+def generateRandNumbers(length):
+    result = []
+    for n in range(length):
+        result.append(round(random.uniform(0, 100), 2))
+    return result
+
+df = pd.DataFrame(columns=['numbers1', 'numbers2', 'numbers3'])
+df['numbers1'] = generateRandNumbers(20)
+df['numbers2'] = generateRandNumbers(20)
+df['numbers3'] = generateRandNumbers(20)
+
+path = 'D:\TommyChan\school\datadrevet\IT3212-Datadrevet-programvare\preprocessing\mockData'
+df.to_csv(path + '\mock.csv')
+
+print(df)
--- preprocessing/normalization.py
+++ preprocessing/normalization.py
@@ -1,1 +1,7 @@
-print('test')
+import pandas as pd
+
+# test with mock data
+# df = pd.read_csv('D:\TommyChan\school\datadrevet\IT3212-Datadrevet-programvare\preprocessing\mockData\mock.csv')
+
+normalized_df=(df-df.min())/(df.max()-df.min())
+print(normalized_df)

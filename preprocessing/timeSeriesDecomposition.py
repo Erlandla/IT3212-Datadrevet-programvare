2a80cf4129e3fed3f2e364cf042df49a3fe1f06b
Tommy Chan <tommychan661995@gmail.com>
2023-10-18 12:55:04 +0200

    add work in progress timeseries decomp

--- preprocessing/timeSeriesDecomposition.py
+++ preprocessing/timeSeriesDecomposition.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 #columnNames = ['Temperature','Humidity','Wind Speed','general diffuse flows','diffuse flows','Zone 1 Power Consumption','Zone 2  Power Consumption','Zone 3  Power Consumption']
@@ -7,10 +8,13 @@ columnNames = ['DateTime']
 
 df = pd.read_csv('D:\TommyChan\school\datadrevet\IT3212-Datadrevet-programvare\Tetuan City power consumption.csv', usecols=columnNames)
 
-print(df)
+print(df['DateTime'].iloc[0])
 
 # Fill missing data
 #df.interpolate(method='linear')
 
+datetime_obj = datetime.strptime(df['DateTime'].iloc[0], '%m/%d/%Y %H:%M')
 
-print(df)
+
+
+print(datetime_obj)
